d86d8b072e109ecb1d4461d0e6289d28c9f411c6
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-08-09 15:32:25 +0300

    create admin panel

--- event_reporting/admin.py
+++ event_reporting/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Event, Customer
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("model", "error", "date" ,"orderNo","customer_list",)
+    search_fields = ("model",)
+
+    def customer_list(self, obj):
+        html = ""
+        for customer in obj.customers.all():
+            html += customer.name + " "
+        return html
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("name", "customer_count",)
+    prepopulated_fields = {"name": ("name",),}
+
+    def customer_count(self, obj):
+        return obj.customer_set.count()
+    
